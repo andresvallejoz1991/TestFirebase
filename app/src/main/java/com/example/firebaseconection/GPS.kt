af454104fba2e69b9cc6fc3389cb7312a882576b
andresvallejoz1991 <m3Tallicaeslaley>
2021-12-13 19:33:55 -0500

    second

---
+++ app/src/main/java/com/example/firebaseconection/GPS.kt
@@ -0,0 +1,127 @@
+package com.example.firebaseconection
+
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Looper
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
+import com.google.firebase.auth.FirebaseAuth
+import io.grpc.InternalChannelz.id
+import kotlinx.android.synthetic.main.activity_gps.*
+import kotlinx.android.synthetic.main.activity_home.*
+import java.util.*
+
+class GPS : AppCompatActivity() {
+    // Declaracion de variables para la locacion
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    val idPermiso = 80
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gps)
+
+
+
+    }
+    // 1. Funcion para validar que los permisos esten dando correctamente
+    private fun verificarPermisos():Boolean{
+        if(
+            ActivityCompat.checkSelfPermission(
+                this,android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+            || ActivityCompat.checkSelfPermission(
+                this,android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        ){
+            return true
+        }
+        return false
+    }
+    // 2. En caso de no tener permisos necesitamos solicitarlos al usuario
+    private fun solicitarPermisos(){
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION),
+            idPermiso
+        )
+    }
+
+    // 3. Validar si la localizacion del dispositivo esta habilitada
+    private fun hablilitarLocacion(): Boolean{
+        var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER)
+    }
+
+    // 4. Funcion que nos permitira obtener la ultima localizacion
+    private fun obtenerUltimaUbicacion(){
+        // Primero chequeamos los pemidos
+
+        if(verificarPermisos()){
+            // Verificamos si la localizacion esta habilitada
+            if(hablilitarLocacion()){
+                // Obtencion de la locaizacion
+
+
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return
+                }
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener{ task ->
+                    var location = task.result
+                    if (location == null){
+                        // Si la locacion es nula obtendremos la locacion de usuario
+                        //actualizaUbicacion()
+                    }else{
+                        var txtLocation = findViewById<TextView>(R.id.txtLocation)
+                        txtLocation.text = "Tus coordenadas actuales son:\nLatutid: " + location.latitude + "\nLongitud: " + location.longitude + "\n" + obtenerCiudad(location.latitude,location.longitude)
+                    }
+                }
+            }else{
+                Toast.makeText(this, "Porfavor habilite la localizacion", Toast.LENGTH_SHORT).show()
+            }
+        }else{
+            solicitarPermisos()
+        }
+
+    }
+
+    // 6. Tenemos la latitud y longitud, gracais a Geocoder transformamos estos valores a ciudad.
+    private fun obtenerCiudad(lat: Double,long: Double):String{
+        var ciudad:String = ""
+        var pais = ""
+        var geoCoder = Geocoder(this, Locale.getDefault())
+        var Adress = geoCoder.getFromLocation(lat,long,3)
+
+        ciudad = Adress.get(0).locality
+        pais = Adress.get(0).countryName
+        return "Lugar: " + ciudad + " Pais: " + pais
+
+
+    }
+
+
+}
--- app/src/main/java/com/example/firebaseconection/HomeActivity.kt
+++ app/src/main/java/com/example/firebaseconection/HomeActivity.kt
@@ -1,6 +1,7 @@
 package com.example.firebaseconection
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.provider.ContactsContract
@@ -8,40 +9,53 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.activity_splash.view.*
 import kotlinx.android.synthetic.main.mylaout.view.*
 
 enum class ProviderType{
-    BASIC
+    BASIC,
+    GOOGLE,
+    FACEBOOK
 }
 
 class HomeActivity : AppCompatActivity(){
     var adapter : GuitarAdapter?=null
     var guitarlist = ArrayList<Guitar>()
+    private val db = FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        guitarlist.add(Guitar("Acustic","Guitarra",R.drawable.acusticguitar))
-        guitarlist.add(Guitar("Electric","Guitarra Electrica",R.drawable.electricguitar))
-        guitarlist.add(Guitar("Bajo Electrico","Bajo Electrico",R.drawable.bass))
-        guitarlist.add(Guitar("Bateria","Bateria",R.drawable.baterry))
-        guitarlist.add(Guitar("Microfono","Micro",R.drawable.micro))
-        guitarlist.add(Guitar("Piano","Piano",R.drawable.piano))
+        //guitarlist.add(Guitar("Acustic","Guitarra",R.drawable.acusticguitar))
+        //guitarlist.add(Guitar("Electric","Guitarra Electrica",R.drawable.electricguitar))
+        //guitarlist.add(Guitar("Bajo Electrico","Bajo Electrico",R.drawable.bass))
+        //guitarlist.add(Guitar("Bateria","Bateria",R.drawable.baterry))
+        //guitarlist.add(Guitar("Microfono","Micro",R.drawable.micro))
+        //guitarlist.add(Guitar("Piano","Piano",R.drawable.piano))
 
 
-        adapter = GuitarAdapter(this, guitarlist)
-        gridview.adapter = adapter
+        //adapter = GuitarAdapter(this, guitarlist)
+        //gridview.adapter = adapter
 
         val bundle:Bundle? = intent.extras
         val email:String? = bundle?.getString("email")
         val provider:String? = bundle?.getString("provider")
 
+        btnGPS.setOnClickListener{
+            val saltar: Intent = Intent(this,GPS::class.java)
+            startActivity(saltar)
+        }
 
+        btnCamara.setOnClickListener{
+            val saltar: Intent = Intent(this,Camara::class.java)
+            startActivity(saltar)
+        }
 
 
         setup(email?:"",provider?:"")
@@ -91,6 +105,25 @@ class HomeActivity : AppCompatActivity(){
             FirebaseAuth.getInstance().signOut()
             onBackPressed()
         }
+
+
+        btn_guardar.setOnClickListener(){
+            db.collection("users").document(email).set(
+                hashMapOf("provider" to provider, "address" to edit_direccion.text.toString(),
+                    "phone" to edit_telefono.text.toString(), "rol" to edit_rol.text.toString())
+            )
+        }
+        btn_recuperar.setOnClickListener(){
+            db.collection("users").document(email).get().addOnSuccessListener {
+                edit_direccion.setText(it.get("address") as String?)
+                edit_telefono.setText(it.get("phone") as String?)
+                edit_rol.setText(it.get("rol") as String?)
+            }
+        }
+        btn_eliminar.setOnClickListener(){
+            db.collection("users").document(email).delete()
+        }
+
 
     }
 }
